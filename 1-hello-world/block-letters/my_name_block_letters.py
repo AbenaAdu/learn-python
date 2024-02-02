062ec2e9bbcf90232d1c121c522889e9473ffc3c
Abena Adu <abeanadu@gmail.com>
2024-02-01 20:12:58 -0500

    Completed assignment

---
+++ 1-hello-world/block-letters/my_name_block_letters.py
@@ -0,0 +1,17 @@
+# Abena Adu
+# I am an INFP
+print("  A  ")
+print(" A A ")
+print("A   A")
+print("AAAAA")
+print("A   A")
+print("A   A")
+print("A   A")
+
+print("BBBB ")
+print("B   B")
+print("B   B")
+print("BBBB ")
+print("B   B")
+print("B   B")
+print("BBBB ")
